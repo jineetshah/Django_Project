58bae38e027fe51134016fd0c1fd7c3d99bb5512
BhagatHarsh <93080554+BhagatHarsh@users.noreply.github.com>
2023-10-06 16:41:23 +0000

    Feat: Added the uAgents initial code for making agents and tested it against the openweather API

--- Django/settings.py
+++ Django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8)lcgcff51ld2qf0m*(=ln8fr7c2oynmrmvjyze#*g9a#3d6=0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangoproject--jineetshah.repl.co']
 
 
 # Application definition
--- agent.py
+++ agent.py
@@ -1,28 +1,39 @@
 from typing import Tuple
 from uagents import Agent, Context
 from fetcher import fetch_temperature, send_alert
+import sys
+
+location = sys.argv[1]
 
 # Create the agent object
-temperature_agent = Agent(name="temperature_agent", seed="your_recovery_phrase")
+temperature_agent = Agent(name="temperature_agent",
+                          seed="your_recovery_phrase")
+
 
 # Define the task for the agent to check the temperature and send alerts
-@temperature_agent.on_interval(period=2)  # Run the task every 60 seconds
-async def check_temperature(ctx: Context, location: str, range: Tuple[int, int]):
+@temperature_agent.on_interval(period=30)  # Run the task every 60 seconds
+async def check_temperature(ctx: Context):
+    global location
     # Fetch the current temperature from the weather API for the specified location
-    min_temperature = 40
-    max_temperature = range[1]
+    min_temperature = 30
+    max_temperature = 40
 
-    temperature = fetch_temperature(location)
+    response = fetch_temperature(location)
+    temperature = float(response["temperature_celsius"])
 
+    print(f"Temperature of the location: {temperature}")
     if temperature < min_temperature:
-        ctx.logger.info(f"Temperature below minimum threshold: {temperature}°C")
+        ctx.logger.info(
+            f"Temperature below minimum threshold: {temperature}°C")
         # Send an alert/notification to the user
         send_alert("Temperature below minimum threshold")
 
     if temperature > max_temperature:
-        ctx.logger.info(f"Temperature above maximum threshold: {temperature}°C")
+        ctx.logger.info(
+            f"Temperature above maximum threshold: {temperature}°C")
         # Send an alert/notification to the user
         send_alert("Temperature above maximum threshold")
+
 
 if __name__ == "__main__":
     temperature_agent.run()
